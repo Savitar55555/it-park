4464252db8113673ac0f58287f9677513ffe7461
Savitar55555 <157879633+Savitar55555@users.noreply.github.com>
2024-07-22 10:39:04 +0600

    main

---
+++ it-park/src/components/Header/header.tsx
@@ -0,0 +1,27 @@
+import styles from "./header.module.css"; 
+import Image from "next/image";
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <div>
+                <Image src="/images/mainImg/itpark.svg" alt="" width={52} height={50} /> 
+            </div>
+            <div className={styles.rightSection}>
+                <div className={styles.button}>
+                    <Image src="/images/headerImg/instagram.svg" alt="f" width={30} height={30} />
+                    <Image src="/images/headerImg/whatsapp.svg" alt="f" width={30} height={30}/>
+                    <a className={styles.text_a} href="">+996(997) 919-170</a>
+                </div>
+                <div className={styles.button}>
+                    <a className={styles.text_a} href="">Записаться на консультацию</a>
+                </div>
+                <div className={styles.button_menu}>
+                    <Image src="/images/mainImg/menu.svg" alt="j" width={48} height={48} />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+  
---
+++ it-park/src/components/MainInfo/info.tsx
@@ -0,0 +1,14 @@
+import Header from "../Header/header";
+import Main from "../Main/main";
+import styles from "./info.module.css"; 
+
+export default function Info() {
+    return (
+        <div>
+            <Header></Header>
+            <Main></Main>
+        </div>
+      
+    );
+}
+  
