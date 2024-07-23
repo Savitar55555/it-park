e740dce9f3044d7dd4820ec026d9f75c1000334a
Savitar55555 <157879633+Savitar55555@users.noreply.github.com>
2024-07-23 20:59:05 +0600

    main finish

--- it-park/src/app/layout.tsx
+++ it-park/src/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet" href="/fonts/sf-pro-display.css" /> 
+        
       </head>
       <body className={golosText.className}>
         {children}
