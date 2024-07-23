8d3bf3183dbd711998e4bef8528c078578ac52a0
Savitar55555 <157879633+Savitar55555@users.noreply.github.com>
2024-07-23 20:59:05 +0600

    main finish

--- it-park/src/components/Main/main.tsx
+++ it-park/src/components/Main/main.tsx
@@ -25,7 +25,7 @@ export default function Main() {
           </div>
           <div className={styles.main_text}>
             <h1 className={styles.main_text_h1}>
-              Передовые технологии обучения        
+              Передовые технологии <br /> обучения        
               <Image className={styles.icons} src="/images/mainImg/icons.svg" alt="d" width={140} height={55} />         
             </h1>
             <p className={styles.main_text_desc}>
@@ -37,11 +37,51 @@ export default function Main() {
               <p className={styles.main_text_scroll}>Скрольте вниз</p> 
             </div>
           </div>
-          <Image className={styles.image} src="/images/mainImg/mainphoto.svg" alt="" width={660} height={700} />
-          <div className={styles.cards}>
-            <div className={styles.card_info}></div>
-            <div className={styles.card_info}></div>
-          </div> 
+          <div className={styles.image_container}>
+            <Image className={styles.image} src="/images/mainImg/mainphoto.svg" alt="" width={660} height={700} />
+            <div className={styles.cards}>
+              <div className={styles.card_info}>
+                <div className={styles.connection}>
+                  <div className={styles.contact_text}>
+                    <Image className={styles.contact} src="/images/mainImg/contact.svg" alt="" width={36} height={36}  />
+                    <div>
+                      Адилет
+                    </div>
+                    <div className={styles.contact_text_1}>
+                      Backend Developer
+                    </div>
+                  </div>
+                  
+                  <div className={styles.link}>
+                    <a href="">
+                      Backend
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.card_info}>
+                <div className={styles.connection}>
+                  <div className={styles.contact_text_2}>                   
+                    <Image className={styles.contact} src="/images/mainImg/telegram.svg" alt="" width={36} height={36} />
+                    <div>
+                      Ознакомтесь направлением Backend и изучите все аспекты
+                    </div>
+                  </div>
+                  <div className={styles.block}>
+                    <div className={styles.link2}>
+                      <a href="">
+                        +3 направления
+                      </a>
+                    </div>
+                    <Image src="/images/mainImg/upper_right_arrow.svg" alt="" width={40} height={40}/>
+                  </div>
+                  
+                </div>
+              </div>
+
+          </div>
+          
+          </div>
         </div>
       </main>
     );
